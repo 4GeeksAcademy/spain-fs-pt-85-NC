e8e43e9563b552d5dc4f12b4a65f5a6f14122738
Nachocordoba13 <nachocordoba34@gmail.com>
2024-10-03 14:28:45 +0000

    excuse generator NC

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#pick").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let suj = Math.floor(Math.random() * who.length);
+  let verb = Math.floor(Math.random() * action.length);
+  let obj = Math.floor(Math.random() * what.length);
+  let time = Math.floor(Math.random() * when.length);
+
+  return who[suj] + " " + action[verb] + " " + what[obj] + " " + when[time];
 };
